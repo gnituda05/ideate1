4076b79f8bbb57a72ef8632886e4064679367ea1
gnituda05 <gnituda05@gmail.com>
2024-01-18 00:56:53 +0800

    Updated the attributes

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -127,9 +127,9 @@ const HomePage: React.FC<HomePageProps> = ({ onNavigate1, onNavigate }) => {
 							<button
 								type="submit"
 								onClick={onNavigate}
-								className=" p-3 w-40 h-12 rounded-2xl bg-blue-600 text-white lg:flex lg:p-5 lg:w-64 lg:h-20 lg:ms-3 lg:rounded-2xl lg:duration-300 lg:transform lg:group-hover:bg-lime-400 lg:gap-2 lg:group-hover:text-black lg:group-hover:w-72 lg:group-hover:ms-0   "
+								className=" p-2 w-40 h-12 rounded-2xl bg-blue-600 text-white lg:flex lg:p-5 lg:w-64 lg:h-20 lg:ms-3 lg:rounded-2xl lg:duration-300 lg:transform lg:group-hover:bg-lime-400 lg:gap-2 lg:group-hover:text-black lg:group-hover:w-72 lg:group-hover:ms-0   "
 							>
-								<p className="flex ms-2 items-center text-base font-semibold font-lato lg:ms-11  lg:mt-1 lg:text-xl lg:font-semibold lg:duration-300 lg:transform lg:group-hover:ms-12 lg:group-hover:text-2xl   ">
+								<p className="flex ms-3 items-center text-base font-semibold font-lato lg:ms-11  lg:mt-1 lg:text-xl lg:font-semibold lg:duration-300 lg:transform lg:group-hover:ms-12 lg:group-hover:text-2xl   ">
 									{" "}
 									work with us<span className="mr-3"></span> <MdArrowOutward />
 								</p>
